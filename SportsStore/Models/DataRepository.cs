2c8932deacbd7561a50245b6652729e07adbaffb
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2018-08-09 10:52:53 +0300

    Add project files.

---
+++ SportsStore/Models/DataRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public class DataRepository : IRepository
+    {
+        private List<Product> data = new List<Product>();
+
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return data;
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            data.Add(product);
+        }
+    }
+}
